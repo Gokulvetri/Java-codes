f1178dc38f863436ffcb2343d2b9f9ec72832a55
gokulvetri <gokulakannan.perumal1302@gmail.com>
2025-02-05 11:24:56 +0530

    collection in Java

---
+++ collectionsdemo.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Collection;
+public class collectionsdemo {
+    public static void main(String[] args) {
+        Collection nums = new ArrayList<>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(4);
+        System.out.print(nums);
+    }
+    
+}
