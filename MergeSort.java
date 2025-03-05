812eeed912fedbc005399716672f7470176cb5ae
Gokulvetri <gokulakannan.perumal1302@gmail.com>
2025-03-05 09:51:05 +0530

    Mergesort

---
+++ MergeSort.java
@@ -0,0 +1,58 @@
+public class MergeSort {
+    public static void mergesort(int[] arr, int l, int r) {
+        int mid = (l + r) / 2;
+        if (l < r) {
+            mergesort(arr, l, mid);
+            mergesort(arr, mid + 1, r);
+
+            merge(arr, l, mid, r);
+        }
+    }
+
+    private static void merge(int[] arr, int l, int mid, int r) {
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
+        int[] lArr = new int[n1];
+        int[] rArr = new int[n2];
+
+        for (int x = 0; x < n1; x++) {
+            lArr[x] = arr[l + x];
+        }
+        for (int x = 0; x<n2;x++){
+            rArr[x] = arr[mid+x+1];
+        }
+        int i =0;
+        int j =0;
+        int k =l;
+        while (i  < n1 && j <n2){
+            if (lArr[i] <= rArr[j]){
+                arr[k]= lArr[i];
+                i++;
+            }
+            else{
+                arr[k] = rArr[j];
+                j++;
+            }
+            k++;
+        }
+        while(i < n1){
+            arr[k] = lArr[i];
+            i++;
+            k++;
+        }
+        while(j <n2){
+            arr[k] = rArr[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] arr ={3,5,8,7,6,1,2,4};
+        mergesort(arr,0,arr.length-1);
+
+        for (int num : arr){
+            System.out.print(num +" ");
+        }
+    }
+}
